92047ab352506b36c868d7acdc54417930af6f7a
s-ong-c <somony9292@gmail.com>
2020-06-12 22:36:49 +0900

    feat: movieApp

---
+++ movieApp/src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {StatusBar} from 'react-native';
+
+const App: React.FC<{}> = () => {
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+    </>
+  );
+};
+
+export default App;
